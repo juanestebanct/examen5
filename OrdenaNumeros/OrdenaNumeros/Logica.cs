dd85bd4a6464f4d8fa5fc96311b9f85f6ffcb2b7
Juan Esteban Ciceri Tobon <juan.ciceri@upb.edu.co>
2021-05-21 19:12:20 -0500

    se añadio la logica sin implementar el form

--- OrdenaNumeros/OrdenaNumeros/Logica.cs
+++ OrdenaNumeros/OrdenaNumeros/Logica.cs
@@ -8,5 +8,50 @@ namespace OrdenaNumeros
 {
     class Logica
     {
+        private int[,] matrizValores;
+        private int posicionFila, posicionColumna;
+
+        public int Posicionfila
+        {
+            get
+            {
+                return posicionFila;
+            }
+            set
+            {
+                posicionFila = value;
+            }
+        }
+        public int PosicionColumna
+        {
+            get
+            {
+                return posicionColumna;
+            }
+            set
+            {
+                posicionColumna = value;
+            }
+        }
+        public int[,] MatrizValoresposicionfila
+        {
+            get
+            {
+                return matrizValores;
+            }
+        }
+
+        public Logica()
+        {
+            iniciavariables();
+        }
+
+        private void iniciavariables()
+        {
+            posicionFila = 0;
+            posicionColumna = 0;
+            matrizValores = new int[4, 4];
+        }
+
     }
 }
